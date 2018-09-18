6d08b189843d2579def59715b6897fd181a96986
Amber Stuart <JJ06867@tn.gov>
2018-09-18 15:14:03 -0500

    completed module

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContext options) : base(options)
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
         }
